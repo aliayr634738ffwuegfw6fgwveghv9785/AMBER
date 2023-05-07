fd8ea77d500d3bee8a0b8099a0673de470d9f463
Lenovo <aliayrsulaimanov@gmail.com>
2023-05-07 09:12:42 +0600

    Initial commit

---
+++ app/src/main/java/com/example/amber/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.amber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+}
